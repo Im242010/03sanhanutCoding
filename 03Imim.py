855517a7e888ab6730424da220fab8aeb52c7e4b
Im242010 <26836@bbw.ac.th>
2026-06-22 09:37:19 +0700

    โปรแกรมคำนวณคะแนน ทบทวนการใช้ if else
    22/6/69
    โปรแกรมคำนวณคะแนน ทบทวนการใช้ if else

---
+++ 03Imim.py
@@ -0,0 +1,19 @@
+print("โปรแกรมคำนวณคะแนนรวม\n")
+
+maths = int(input("คะแนนสอบคณิตศาสตร์"))
+English = int(input("คะแนนสอบภาษาอังกฤษ"))
+Social = int(input("คะแนนสอบสังคม"))
+
+total_score = maths + English + Social
+average = total_score/3
+if average < 60:
+    print("คะแนนรวม 3 วิชา") , total_score
+    print("คะแนนเฉลี่ย 3 วิชา") , average
+    print("ควรปรับปรุง")
+elif average > 80:
+    print("ผ่าน")
+else:
+    print("คะแนนรวม 3 วิชา")
+    print("คะแนนเฉลี่ย 3 วิชา",total_score/3)
+    print("ดีเยี่ยม")
+print("โดยนายสัณหณัฐ วีรบรรจง ม.4/4 เลขที่3")

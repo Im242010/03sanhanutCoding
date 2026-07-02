e93396cc3da86b7ee2ad5f9b33c4ea6ec61b6d44
Im242010 <26836@bbw.ac.th>
2026-07-02 14:20:37 +0700

    ฝึกเขียนโปรแกรมการใช้loop
    2/7/69ฝึกเขียนโปรแกรมการใช้loop

---
+++ Imloop03.py
@@ -0,0 +1,19 @@
+import random
+my_number = random.randint(1,100)
+while True:
+    try:
+        guess = int(input("ตัวเลขของคุณ: "))
+        attempts = 0
+        attempts += 1
+        
+        if  guess < my_number:
+            print("น้อยไป")
+        elif guess > my_number:
+            print("เกินไปจ้า")
+        else:
+            print(f"ถูกต้อง คำตอบคือ{my_number}")
+            print(f"คุณใช้จำนวนการทายทั้งหมด {attempts} ครั้ง")
+            break
+
+    except ValueError:
+        print("ใส่เลขที่ถูกต้อง")
